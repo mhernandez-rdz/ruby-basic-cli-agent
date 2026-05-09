1eb5d2e2c057d0a0506b9aeec51fa968326f0b3f
mhernandez-rdz <mhbyakkoiv@gmail.com>
2026-05-08 19:08:12 -0600

    finish agent chat loop and adds edit_file tool

---
+++ lib/tools/edit_file.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class EditFile < Tool # :nodoc:
+  def call(args)
+    content = File.read(args['path'])
+    updated = content.sub(args['old_str'], args['new_str'])
+    File.write(args['path'], updated)
+    "OK"
+  end
+
+  class << self
+    def tool_name
+      "edit_file"
+    end
+
+    def schema
+      {
+        type: 'function',
+        function: {
+          name: tool_name,
+          description: 'Edits content of a given file',
+          parameters: {
+            type: 'object',
+            properties: {
+              path: { type: 'string', description: 'File path' },
+              old_str: { type: 'string', description: 'Text to replace' },
+              new_str: { type: 'string', description: 'New text' }
+            },
+            required: ['path', 'old_str', 'new_str']
+          }
+        }
+      }
+    end
+  end
+end
