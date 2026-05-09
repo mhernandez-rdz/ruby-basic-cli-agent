3023991d2bc51320c21cad6807ba352dc3787e42
mhernandez-rdz <mhbyakkoiv@gmail.com>
2026-05-08 19:22:24 -0600

    Adds run_command tool to run system utilities

---
+++ lib/tools/run_command.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'open3'
+
+class RunCommand < Tool # :nodoc:
+  def call(args)
+    stdout, stderr, _status = Open3.capture3(args['command'])
+    stdout.empty? ? stderr : stdout
+  end
+
+  class << self
+    def tool_name
+      "run_command"
+    end
+
+    def schema
+      {
+        type: 'function',
+        function: {
+          name: tool_name,
+          description: 'Run system commands',
+          parameters: {
+            type: 'object',
+            properties: {
+              command: { type: 'string', description: 'Command to run' },
+            },
+            required: ['command']
+          }
+        }
+      }
+    end
+  end
+end
