ba798299dffc2ca08837f1436e84930066371f4b
mhernandez-rdz <mhbyakkoiv@gmail.com>
2026-05-23 10:26:03 -0600

    adds minitest reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require 'minitest/reporters'
 require 'tmpdir'
 require 'tempfile'
 require 'fileutils'
@@ -16,3 +17,5 @@ require 'tools/list_dir'
 require 'tools/write_file'
 require 'tools/edit_file'
 require 'tools/run_command'
+
+Minitest::Reporters.use!([Minitest::Reporters::DefaultReporter.new(color: true)])
